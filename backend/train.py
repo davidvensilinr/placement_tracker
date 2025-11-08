748fb63f32d3a0a0d6be842240f07abde233b819
davidvensilinr <relaxbro2005@gmail.com>
2025-11-08 16:41:41 +0530

    training the logistic regression model

---
+++ backend/train.py
@@ -0,0 +1,18 @@
+from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,classification_report
+import pickle
+
+df=pd.read_csv("dataset/dsa_placement_dataset.csv")
+
+X,y=df[["Arrays","Heaps","Graphs","DynamicProgramming"]],df["Probability_of_Placement"]
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=42,test_size=0.2)
+model=LogisticRegression()
+model.fit(X_train,y_train)
+print(accuracy_score(y_test,model.predict(X_test)))
+
+with open("model.pkl","wb") as f:
+    pickle.dump(model,f)
+print("model is successfully trained")
