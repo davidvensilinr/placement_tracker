e45ec43a29461a8b04709d067dc960dd9f6f8ffe
davidvensilinr <relaxbro2005@gmail.com>
2025-11-08 15:32:59 +0530

    backend setup

---
+++ backend/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI,Request
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+app=FastAPI()
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["https://localhost:3000"],
+allow_credentials=True,
+allow_methods=['*'],
+allow_headers=['*']
+)
+
+@app.post('/predict')
+async def predict(request:Request):
+    data = await request.json()
+    print(data)
+    return {"message":"something"}
